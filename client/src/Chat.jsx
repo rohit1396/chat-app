50c6bfba004ac88fa56629a9a50d186b55420ecd
rohit1396 <rohitgaikwad726@gmail.com>
2024-02-09 20:34:47 +0530

    tailwind styling

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { IoMdSend } from "react-icons/io";
+import ScrollToBottom from "react-scroll-to-bottom";
 
 const Chat = ({ socket, userName, room }) => {
   const [curentMessage, setCurrentMessage] = useState("");
@@ -21,6 +23,7 @@ const Chat = ({ socket, userName, room }) => {
       };
       await socket.emit("send_message", messageData);
       setMessageDetails((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
 
@@ -36,35 +39,62 @@ const Chat = ({ socket, userName, room }) => {
 
   return (
     <div className="h-4/6 w-1/4 border rounded-md bg-cyan-100 flex flex-col relative">
+      {/* Chat Header */}
       <div className="w-full h-16 bg-cyan-200 text-slate-50 text-4xl font-bold text-center flex items-center px-5">
         Chat Header
       </div>
-      <div className="w-full h-3/4 border border-red-400 overflow-y-scroll">
-        {messageDetails.map((item) => {
-          return (
-            <div
-              key={item.id}
-              className="max-w-4/5 w-fit h-auto min-h-10 bg-cyan-400 rounded-md mx-2 my-2 py-1 px-4"
-            >
-              <p>{item.message}</p>
-              <div>{item.time}</div>
-            </div>
-          );
-        })}
+      {/* Chat Body */}
+      <div className="w-full h-3/4 overflow-y-auto">
+        <ScrollToBottom className="w-full h-full">
+          {messageDetails.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="max-w-4/5 w-fit h-auto min-h-10 relative my-4"
+              >
+                <div
+                  className={`border border-2 border-red-400 flex w-11/12 min-h-10 text-slate-50 text-base font-light rounded-md mx-2 my-2 py-1 px-3 flex ${
+                    userName === item.author
+                      ? "bg-cyan-400 justify-center"
+                      : "bg-emerald-400 justify-end"
+                  } `}
+                >
+                  {/* message */}
+                  <div
+                  // className={`w-11/12 min-h-10 text-slate-50 text-base font-light rounded-md mx-2 my-2 py-1 px-3 flex ${
+                  //   userName === item.author
+                  //     ? "bg-cyan-400 justify-center"
+                  //     : "bg-emerald-400 justify-end"
+                  // } `}
+                  >
+                    <p>{item.message}</p>
+                  </div>
+                  {/* message details */}
+                  <div className="w-max absolute bottom-[-8px] left-2 text-xs font-thin flex">
+                    <p className="mx-1">{item.time}</p>
+                    <p>{item.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
       </div>
-      <div className="w-full my-1 mx-1 absolute bottom-0 text-start pl-2">
+      {/* Chat Footer */}
+      <div className="w-full bg-cyan-400 border-t py-1 px-1 absolute bottom-0 text-start pl-2 flex justify-around">
         <input
           type="text"
-          placeholder="Enter Room Id..."
+          placeholder="Type Message Here..."
+          value={curentMessage}
           onChange={(e) => setCurrentMessage(e.target.value)}
-          className="h-12 w-3/4"
+          className="h-12 w-4/5 rounded-3xl px-3 outline-none border border-slate-100"
         />
-        <button
-          onClick={sendMessage}
-          className="bg-emerald-600 h-12 w-1/5 mx-0 my-auto"
-        >
-          Send
-        </button>
+        <div className="w-1/6 h-12 rounded-full bg-slate-50 flex justify-center items-center mx-2 my-auto">
+          <IoMdSend
+            onClick={sendMessage}
+            className=" text-cyan-500 w-4/6 h-4/6 cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
